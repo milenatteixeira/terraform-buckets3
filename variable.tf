08a0d0d872441abe354509f0fa4a28872c8d6f14
Milena Teixeira <milenateixeira648@gmail.com>
2021-08-16 16:23:57 -0300

    examble project published

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+    default = "<chave-de-acesso>"
+}
+
+variable "aws_secret_key" {
+    default = "<chave-da-senha>"
+}
+variable "region" {
+    default = "<regiao>"
+}
